e3a15825af8fb781eabeeee88c36c41c797d5e8c
Antoine Colmard <a.colmard@gmail.com>
2018-05-21 23:45:56 +0200

    :bug: Fix floating label not using children

--- src/components/mdc-floating-label/index.js
+++ src/components/mdc-floating-label/index.js
@@ -3,8 +3,8 @@ import { MDCFloatingLabel } from '@material/floating-label';
 
 import { withMaterialComponent } from '../with-material-component';
 
-export function FloatingLabelBase() {
-	return <label className="mdc-floating-label" />;
+export function FloatingLabelBase({ children }) {
+	return <label className="mdc-floating-label">{children}</label>;
 }
 
 export const FloatingLabel = withMaterialComponent(FloatingLabelBase, MDCFloatingLabel);
