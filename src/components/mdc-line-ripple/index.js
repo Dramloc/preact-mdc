977c234f948e554a48363b9487a15e4ac6e32ddf
Antoine Colmard <a.colmard@gmail.com>
2018-05-21 23:33:42 +0200

    :sparkles: Add mdc-line-ripple

---
+++ src/components/mdc-line-ripple/index.js
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+import { MDCLineRipple } from '@material/line-ripple';
+
+import { withMaterialComponent } from '../with-material-component';
+
+export function LineRippleBase() {
+	return <div className="mdc-line-ripple" />;
+}
+
+export const LineRipple = withMaterialComponent(LineRippleBase, MDCLineRipple);
