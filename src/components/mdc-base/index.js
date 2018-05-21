51930f8e774c775aa93b4ff1d2e6635bc18301f4
Antoine Colmard <a.colmard@gmail.com>
2018-05-21 16:06:51 +0200

    :hammer: Pass children as prop in Base

--- src/components/mdc-base/index.js
+++ src/components/mdc-base/index.js
@@ -1,10 +1,8 @@
 import { h } from 'preact';
 import cx from 'bem-classnames';
 
-export function Base({ className, children, classes, element: Element, modifiers, ...props }) {
+export function Base({ className, classes, element: Element, modifiers, ...props }) {
 	return (
-		<Element className={cx(classes, modifiers, className)} {...props}>
-			{children}
-		</Element>
+		<Element className={cx(classes, modifiers, className)} {...props} />
 	);
 }
