12526c2113ef19295086dd655752ca6c912f239f
Antoine Colmard <a.colmard@gmail.com>
2018-05-21 21:05:10 +0200

    :hammer: Move checkmark children from props to... children

--- src/components/mdc-chip/index.js
+++ src/components/mdc-chip/index.js
@@ -32,21 +32,16 @@ export function ChipIcon(props) {
 
 export function ChipCheckmark(props) {
 	return (
-		<Base
-			element="div"
-			classes={{ name: 'mdc-chip__checkmark' }}
-			{...props}
-			children={
-				<svg className="mdc-chip__checkmark-svg" viewBox="-2 -3 30 30">
-					<path
-						element="path"
-						className="mdc-chip__checkmark-path"
-						fill="none"
-						stroke="black"
-						d="M1.73,12.91 8.1,19.28 22.79,4.59"
-					/>
-				</svg>
-			}
-		/>
+		<Base element="div" classes={{ name: 'mdc-chip__checkmark' }} {...props}>
+			<svg className="mdc-chip__checkmark-svg" viewBox="-2 -3 30 30">
+				<path
+					element="path"
+					className="mdc-chip__checkmark-path"
+					fill="none"
+					stroke="black"
+					d="M1.73,12.91 8.1,19.28 22.79,4.59"
+				/>
+			</svg>
+		</Base>
 	);
 }
