e78e009c9b7cb7b6eda8e23d87222c6b19bcc9cc
Antoine Colmard <a.colmard@gmail.com>
2018-05-23 22:41:40 +0200

    :sparkles: Add mdc-shape

--- src/routes/shape/index.js
+++ src/routes/shape/index.js
@@ -1,12 +1,137 @@
 import { h } from 'preact';
+import cx from 'classnames';
+
+import { Button } from '../../components/mdc-button';
+import {
+	Card,
+	CardPrimaryAction,
+	CardActions,
+	CardActionIcons,
+	withCardAction
+} from '../../components/mdc-card';
+import { IconToggle } from '../../components/mdc-icon-toggle';
+import { RippledIcon } from '../../components/mdc-icon';
+import { ShapeContainer, ShapeContainerCorner } from '../../components/mdc-shape';
 import Showcase from '../../components/showcase';
 
+import cardStyle from '../card/style.scss';
+import style from './style.scss';
+
+const ActionIconToggle = withCardAction(IconToggle);
+const ActionIcon = withCardAction(RippledIcon);
+
+function DemoCardPrimary() {
+	return (
+		<div className={cardStyle['demo-card__primary']}>
+			<h2 className={cx(cardStyle['demo-card__title'], 'mdc-typography--headline6')}>
+				Our Changing Planet
+			</h2>
+			<h3 className={cx(cardStyle['demo-card__subtitle'], 'mdc-typography--subtitle2')}>
+				by Kurt Wagner
+			</h3>
+		</div>
+	);
+}
+
+function DemoCardSecondary() {
+	return (
+		<div className={cx(cardStyle['demo-card__secondary'], 'mdc-typography--body2')}>
+			Visit ten places on our planet that are undergoing the biggest changes today.
+		</div>
+	);
+}
+
+function DemoCardActions() {
+	return (
+		<CardActions>
+			<CardActionIcons>
+				<ActionIconToggle
+					modifiers={{ icon: true }}
+					aria-pressed="false"
+					aria-label="Add to favorites"
+					data-toggle-on={{
+						content: 'favorite',
+						label: 'Remove from favorites'
+					}}
+					data-toggle-off={{
+						content: 'favorite_border',
+						label: 'Add to favorites'
+					}}
+				>
+					favorite_border
+				</ActionIconToggle>
+				<ActionIcon modifiers={{ icon: true }} title="Share">
+					share
+				</ActionIcon>
+				<ActionIcon modifiers={{ icon: true }} title="More options">
+					more_vert
+				</ActionIcon>
+			</CardActionIcons>
+		</CardActions>
+	);
+}
+
 function ShapeHero() {
-	return null;
+	return (
+		<ShapeContainer className={style['hero-shape-container']}>
+			<Button modifiers={{ unelevated: true }}>Angled Corners</Button>
+			<ShapeContainerCorner modifiers={{ 'top-left': true }} />
+			<ShapeContainerCorner modifiers={{ 'top-right': true }} />
+			<ShapeContainerCorner modifiers={{ 'bottom-left': true }} />
+			<ShapeContainerCorner modifiers={{ 'bottom-right': true }} />
+		</ShapeContainer>
+	);
 }
 
 function ShapeDemos() {
-	return null;
+	return (
+		<div>
+			<Showcase.Section>
+				<Showcase.Section.Title>Contained Button</Showcase.Section.Title>
+				<ShapeContainer className={style['contained-button-shape-container']}>
+					<Button modifiers={{ unelevated: true }}>Skip</Button>
+					<ShapeContainerCorner modifiers={{ 'top-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-right': true }} />
+				</ShapeContainer>
+				<ShapeContainer className={style['contained-button-shape-container']}>
+					<Button modifiers={{ unelevated: true }}>Finish</Button>
+					<ShapeContainerCorner modifiers={{ 'top-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'top-right': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-right': true }} />
+				</ShapeContainer>
+			</Showcase.Section>
+			<Showcase.Section>
+				<Showcase.Section.Title>Contained Button</Showcase.Section.Title>
+				<ShapeContainer className={style['outlined-button-shape-container']}>
+					<Button modifiers={{ outlined: true }}>Skip</Button>
+					<ShapeContainerCorner modifiers={{ 'top-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-right': true }} />
+				</ShapeContainer>
+				<ShapeContainer className={style['outlined-button-shape-container']}>
+					<Button modifiers={{ outlined: true }}>Finish</Button>
+					<ShapeContainerCorner modifiers={{ 'top-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'top-right': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-left': true }} />
+					<ShapeContainerCorner modifiers={{ 'bottom-right': true }} />
+				</ShapeContainer>
+				<Showcase.Section>
+					<Showcase.Section.Title>Outlined</Showcase.Section.Title>
+					<ShapeContainer className={style['card-shape-container']}>
+						<Card className={style['card-shape-container__card']} modifiers={{ outlined: true }}>
+							<CardPrimaryAction>
+								<DemoCardPrimary />
+								<DemoCardSecondary />
+							</CardPrimaryAction>
+							<DemoCardActions />
+						</Card>
+						<ShapeContainerCorner modifiers={{ 'top-right': true }} />
+						<ShapeContainerCorner modifiers={{ 'bottom-left': true }} />
+					</ShapeContainer>
+				</Showcase.Section>
+			</Showcase.Section>
+		</div>
+	);
 }
 
 export default function ShapeShowcase() {
