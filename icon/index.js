74c5297c42443274000f16864b554a27b9c9f85c
Antoine Colmard <a.colmard@gmail.com>
2018-05-26 12:58:15 +0200

    :alien: Add future deprecation notices

--- icon/index.js
+++ icon/index.js
@@ -6,4 +6,5 @@ export function Icon({ className, ...props }) {
 	return <i className={cx('material-icons', className)} {...props} />;
 }
 
+// FIXME: replace with icon-button (https://github.com/material-components/material-components-web/commit/39a481597f87e872caf92ef31249279bce665c07)
 export const RippledIcon = withUnboundedSurfaceRipple(Icon);
