3b303b8c1baa81f11f47055d25a95353bc2f363e
Antoine Colmard <a.colmard@gmail.com>
2018-05-27 18:18:11 +0200

    :hammer: Wrap list demos in Showcase.Demo

--- docs/src/routes/list/index.js
+++ docs/src/routes/list/index.js
@@ -1,5 +1,15 @@
 import { h } from 'preact';
-import { List, ListItem, ListItemText, ListItemSecondaryText, ListItemGraphic, ListItemMeta, ListDivider, ListGroup, ListGroupSubheader } from '@preact/mdc/list';
+import {
+	List,
+	ListItem,
+	ListItemText,
+	ListItemSecondaryText,
+	ListItemGraphic,
+	ListItemMeta,
+	ListDivider,
+	ListGroup,
+	ListGroupSubheader
+} from '@preact/mdc/list';
 import cx from 'classnames';
 
 import '@material/list/mdc-list.scss';
@@ -10,7 +20,7 @@ import style from './style.scss';
 
 function ListHero() {
 	return (
-		<div className={style['hero-list']}>
+		<Showcase.Demo className={style['hero-list']}>
 			<List className={style['demo-list']}>
 				<ListItem>
 					<ListItemText>Line item</ListItemText>
@@ -22,7 +32,7 @@ function ListHero() {
 					<ListItemText>Line item</ListItemText>
 				</ListItem>
 			</List>
-		</div>
+		</Showcase.Demo>
 	);
 }
 
@@ -31,135 +41,147 @@ function ListDemos() {
 		<div>
 			<Showcase.Section>
 				<Showcase.Section.Title>Single-Line</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Two-Line</Showcase.Section.Title>
-				<List className={style['demo-list']} modifiers={{ 'two-line': true }}>
-					<ListItem>
-						<ListItemText>
-							Line item
-							<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
-						</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>
-							Line item
-							<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
-						</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>
-							Line item
-							<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
-						</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']} modifiers={{ 'two-line': true }}>
+						<ListItem>
+							<ListItemText>
+								Line item
+								<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
+							</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>
+								Line item
+								<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
+							</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>
+								Line item
+								<ListItemSecondaryText>Secondary text</ListItemSecondaryText>
+							</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Leading Icon</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem>
-						<ListItemGraphic>wifi</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemGraphic>bluetooth</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemGraphic>data_usage</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem>
+							<ListItemGraphic>wifi</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemGraphic>bluetooth</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemGraphic>data_usage</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Trailing Icon</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>
 					Two-Line with Leading and Trailing Icon and Divider
 				</Showcase.Section.Title>
-				<List
-					className={cx(style['demo-list'], style['demo-list--avatar-list'])}
-					modifiers={{ 'two-line': true, 'avatar-list': true }}
-				>
-					<ListItem>
-						<ListItemGraphic>folder</ListItemGraphic>
-						<ListItemText>
-							Dog Photos
-							<ListItemSecondaryText>9 Jan 2018</ListItemSecondaryText>
-						</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-					<ListItem>
-						<ListItemGraphic>folder</ListItemGraphic>
-						<ListItemText>
-							Cat Photos
-							<ListItemSecondaryText>22 Dec 2017</ListItemSecondaryText>
-						</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-					<ListDivider />
-					<ListItem>
-						<ListItemGraphic className={style['demo-list-item__graphic']}>folder</ListItemGraphic>
-						<ListItemText>
-							Potatoes
-							<ListItemSecondaryText>30 Nov 2017</ListItemSecondaryText>
-						</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-					<ListItem>
-						<ListItemGraphic className={style['demo-list-item__graphic']}>folder</ListItemGraphic>
-						<ListItemText>
-							Carrots
-							<ListItemSecondaryText>17 Oct 2017</ListItemSecondaryText>
-						</ListItemText>
-						<ListItemMeta>info</ListItemMeta>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List
+						className={cx(style['demo-list'], style['demo-list--avatar-list'])}
+						modifiers={{ 'two-line': true, 'avatar-list': true }}
+					>
+						<ListItem>
+							<ListItemGraphic>folder</ListItemGraphic>
+							<ListItemText>
+								Dog Photos
+								<ListItemSecondaryText>9 Jan 2018</ListItemSecondaryText>
+							</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+						<ListItem>
+							<ListItemGraphic>folder</ListItemGraphic>
+							<ListItemText>
+								Cat Photos
+								<ListItemSecondaryText>22 Dec 2017</ListItemSecondaryText>
+							</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+						<ListDivider />
+						<ListItem>
+							<ListItemGraphic className={style['demo-list-item__graphic']}>folder</ListItemGraphic>
+							<ListItemText>
+								Potatoes
+								<ListItemSecondaryText>30 Nov 2017</ListItemSecondaryText>
+							</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+						<ListItem>
+							<ListItemGraphic className={style['demo-list-item__graphic']}>folder</ListItemGraphic>
+							<ListItemText>
+								Carrots
+								<ListItemSecondaryText>17 Oct 2017</ListItemSecondaryText>
+							</ListItemText>
+							<ListItemMeta>info</ListItemMeta>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Dense</Showcase.Section.Title>
-				<List className={style['demo-list']} modifiers={{ dense: true }}>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']} modifiers={{ dense: true }}>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Groups</Showcase.Section.Title>
-				<div className={style['demo-list']}>
+				<Showcase.Demo className={style['demo-list']}>
 					<ListGroup>
 						<ListGroupSubheader>Group 1</ListGroupSubheader>
 						<List>
@@ -189,54 +211,60 @@ function ListDemos() {
 							</ListItem>
 						</List>
 					</ListGroup>
-				</div>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Padded Divider</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListDivider modifiers={{ padded: true }} />
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListDivider modifiers={{ padded: true }} />
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Inset Divider</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem>
-						<ListItemGraphic>wifi</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemGraphic>bluetooth</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListDivider modifiers={{ inset: true }} />
-					<ListItem>
-						<ListItemGraphic>data_usage</ListItemGraphic>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem>
+							<ListItemGraphic>wifi</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemGraphic>bluetooth</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListDivider modifiers={{ inset: true }} />
+						<ListItem>
+							<ListItemGraphic>data_usage</ListItemGraphic>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Activated Item</Showcase.Section.Title>
-				<List className={style['demo-list']}>
-					<ListItem modifiers={{ activated: true }}>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-					<ListItem>
-						<ListItemText>Line item</ListItemText>
-					</ListItem>
-				</List>
+				<Showcase.Demo>
+					<List className={style['demo-list']}>
+						<ListItem modifiers={{ activated: true }}>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+						<ListItem>
+							<ListItemText>Line item</ListItemText>
+						</ListItem>
+					</List>
+				</Showcase.Demo>
 			</Showcase.Section>
 		</div>
 	);
