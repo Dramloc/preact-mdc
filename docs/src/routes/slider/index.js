b265aec9089ae1e8edfd30c9e5708f1d7097ec43
Antoine Colmard <a.colmard@gmail.com>
2018-05-27 18:24:28 +0200

    :hammer: Wrap slider demos in Showcase.Demo

--- docs/src/routes/slider/index.js
+++ docs/src/routes/slider/index.js
@@ -6,7 +6,11 @@ import '@material/slider/mdc-slider.scss';
 import Showcase from '../../components/showcase';
 
 function SliderHero() {
-	return <Slider min={0} max={100} value={50} aria-label="Select Value" />;
+	return (
+		<Showcase.Demo>
+			<Slider min={0} max={100} value={50} aria-label="Select Value" />
+		</Showcase.Demo>
+	);
 }
 
 function SliderDemos() {
@@ -14,42 +18,52 @@ function SliderDemos() {
 		<div>
 			<Showcase.Section>
 				<Showcase.Section.Title>Continuous</Showcase.Section.Title>
-				<Slider min={0} max={50} value={25} aria-label="Select Value" />
+				<Showcase.Demo>
+					<Slider min={0} max={50} value={25} aria-label="Select Value" />
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Discrete</Showcase.Section.Title>
-				<Slider
-					modifiers={{ discrete: true }}
-					min={0}
-					max={50}
-					value={25}
-					aria-label="Select Value"
-				/>
+				<Showcase.Demo>
+					<Slider
+						modifiers={{ discrete: true }}
+						min={0}
+						max={50}
+						value={25}
+						aria-label="Select Value"
+					/>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Custom Step</Showcase.Section.Title>
-				<Slider
-					modifiers={{ discrete: true }}
-					min={0}
-					max={50}
-					value={25}
-					step={5}
-					aria-label="Select Value"
-				/>
+				<Showcase.Demo>
+					<Slider
+						modifiers={{ discrete: true }}
+						min={0}
+						max={50}
+						value={25}
+						step={5}
+						aria-label="Select Value"
+					/>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Discrete with Tick Marks</Showcase.Section.Title>
-				<Slider
-					modifiers={{ discrete: true, 'display-markers': true }}
-					min={0}
-					max={50}
-					value={25}
-					aria-label="Select Value"
-				/>
+				<Showcase.Demo>
+					<Slider
+						modifiers={{ discrete: true, 'display-markers': true }}
+						min={0}
+						max={50}
+						value={25}
+						aria-label="Select Value"
+					/>
+				</Showcase.Demo>
 			</Showcase.Section>
 			<Showcase.Section>
 				<Showcase.Section.Title>Disabled</Showcase.Section.Title>
-				<Slider min={0} max={50} value={25} disabled aria-label="Select Value" />
+				<Showcase.Demo>
+					<Slider min={0} max={50} value={25} disabled aria-label="Select Value" />
+				</Showcase.Demo>
 			</Showcase.Section>
 		</div>
 	);
